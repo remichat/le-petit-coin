2a50a700eaab338923064806072572e5ef7c7572
ronan querrec <ronan_querrec@yahoo.com>
2019-11-19 17:24:38 +0100

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :toilets, only: [:index, :new, :create, :update, :edit, :show]
+  resources :toilets, only: [:index, :new, :create, :update, :edit, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: :index
+
   root to: 'toilets#index'
-  resources :bookings, only: [:index, :new, :create]
 end
