07078b4b9a72b87961b64b1c632be4ae084311c1
Rémi Chatenay <remi.chatenay@gadz.org>
2019-11-19 15:40:52 +0100

    added 2 steps toilet creation

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -3,7 +3,11 @@ class Toilet < ApplicationRecord
   TOILET_TYPES = %w[Turc Sèche Pot]
   belongs_to :user
 
-  validates :title, :description, :address, :toilet_type, :capacity, :gender, presence: true
+  # validates :title, :description, :address, :toilet_type, :capacity, :gender, presence: true
   validates :gender, inclusion: { in: GENDERS }
   validates :toilet_type, inclusion: { in: TOILET_TYPES }
+
+  def finished?
+    toilet_type.present? && description.present?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :toilets, only: [:index, :new, :create]
+  resources :toilets, only: [:index, :new, :create, :update, :edit]
   root to: 'toilets#index'
 end
