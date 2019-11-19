b422cdcddb3bb95322cd3ee2234920b8e9e4cbf3
Hadrien Dalla Torre <hadrien.dallatorre@gmail.com>
2019-11-19 16:47:29 +0100

    booking feature ok + saved schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_11_19_145853) do
 
   # These are extensions that must be enabled in order to support this database
@@ -35,6 +34,18 @@ ActiveRecord::Schema.define(version: 2019_11_19_145853) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "status"
+    t.bigint "toilet_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["toilet_id"], name: "index_bookings_on_toilet_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "toilets", force: :cascade do |t|
@@ -69,5 +80,6 @@ ActiveRecord::Schema.define(version: 2019_11_19_145853) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "toilets"
   add_foreign_key "toilets", "users"
 end
