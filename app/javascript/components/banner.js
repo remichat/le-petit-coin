4679c0ff0ae3cdc015a8d7a81d170e5f0f2298f9
Rémi Chatenay <remi.chatenay@gadz.org>
2019-11-19 11:31:23 +0100

    improved logo and border radius implementation plus added new toilet page

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,13 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["Find the toilet you need", "No more pant accident", "Make the future of toilet"],
-    typeSpeed: 50,
-    loop: true
-  });
+  if (document.getElementById('banner-typed-text')) {
+    new Typed('#banner-typed-text', {
+      strings: ["Find the toilet you need", "No more pant accident", "Make the future of toilet"],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
 }
 
 export { loadDynamicBannerText };
