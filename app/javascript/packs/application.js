2ec1b3c06adbcae418745c368ea0cdfe45b73091
Hadrien Dalla Torre <hadrien.dallatorre@gmail.com>
2019-11-20 16:19:09 +0100

    animation on scroll added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,10 @@
 import "bootstrap";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import { loadDynamicBannerText } from '../components/banner';
 import { toiletTypeToSelect } from '../components/select';
 import { setDatePicker } from "../plugins/flatpickr"
 loadDynamicBannerText();
 toiletTypeToSelect();
 setDatePicker();
+AOS.init();
