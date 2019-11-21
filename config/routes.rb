87b47c6521538c9715c4bf03418978065ab0311c
Jules Bayon de Noyer <julesbn@hotmail.fr>
2019-11-21 13:17:05 +0100

    working on the bookings index front

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :toilets, only: [:index, :new, :create, :update, :edit, :show] do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create,]
   end
 
-  resources :bookings, only: :index
+  resources :bookings, only: [:index, :destroy]
 
   root to: 'toilets#index'
 end
